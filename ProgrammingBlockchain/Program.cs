1449af7c96e434f8353e4dad6230553c326ca732
BarryBurke <barry.burke@gmail.com>
2016-05-12 21:16:12 +0100

    Add project files.

---
+++ ProgrammingBlockchain/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using ProgrammingBlockchain.Chapters;
+
+namespace ProgrammingBlockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chapter = new Chapter1();
+
+            chapter.Lesson1();
+            chapter.Lesson2();
+            chapter.Lesson3();
+            //chapter.lesson4();
+
+            Console.WriteLine("\n\n\nPress enter to continue");
+            Console.ReadLine();
+        }
+    }
+}
